b29f9b1cea78a35d6e67d57bf78f195aad6154e4
abrormirzaabdullaev711-commits <abrormirzaabdullaev711@gmail.com>
2026-07-15 19:13:08 +0500

    Abrorbek

--- src/components/furniture/Furniture.jsx
+++ src/components/furniture/Furniture.jsx
@@ -2,8 +2,12 @@ import "./Furniture.css"
 
 function Furniture() {
   return (
-    <div>
-      <h2>Abrorbek</h2>
+    <div className="wrapper-container">
+      <h2>Share your setup with</h2>
+      <h1>#FuniroFurniture</h1>
+
+      <img src='Images.png' alt="" />
+    
     </div>
   )
 }
--- src/components/room/Room.jsx
+++ src/components/room/Room.jsx
@@ -1,11 +1,43 @@
-import "./Room.css"
+import './Room.css'
 
 function Room() {
   return (
-    <div>
-      <h2>Abrorbek</h2>
-    </div>
-  )
+     <section className="room">
+      <div className="room-text">
+        <h2>
+          50+ Beautiful rooms
+          <br />
+          inspiration
+        </h2>
+
+        <p>
+          Our designer already made a lot of beautiful
+          <br />
+          prototype of rooms that inspire you
+        </p>
+
+        <button>Explore More</button>
+      </div>
+
+
+        <div className="room-card">
+          <img src="/room1.png" alt="" />
+
+          <div className="room-info">
+            <span>01 — Bed Room</span>
+            <h3>Inner Peace</h3>
+            <button>→</button>
+          </div>
+        </div>
+<div className="img-wrapper">
+        <div className="room-card">
+          <img src="/room2.png" alt="" />
+        </div>
+
+        <button className="next">👉</button>
+      </div>
+    </section>
+  );
 }
 
 export default Room
